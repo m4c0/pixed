cc5d4de30e5487dae0043f0558668c3774e92a70
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-03 11:49:41 +0100

    preparing for dynamic imaging

--- pixed.cpp
+++ pixed.cpp
@@ -2,10 +2,23 @@
 
 import casein;
 import quack;
+import traits;
 import voo;
 
+using namespace traits::ints;
+
+static void update_atlas(voo::h2l_image *img) {
+  voo::mapmem m{img->host_memory()};
+  auto *buf = static_cast<uint32_t *>(*m);
+  for (auto y = 0; y < 16; y++) {
+    for (auto x = 0; x < 16; x++, buf++) {
+      *buf = 0U;
+    }
+  }
+}
 static quack::donald::atlas_t *atlas(voo::device_and_queue *dq) {
-  return new voo::sires_image{"input.png", dq};
+  return new quack::donald::atlas_t{dq->queue(), &update_atlas,
+                                    dq->physical_device(), 16U, 16U};
 }
 
 static unsigned quads_4x4(quack::mapped_buffers all) {
