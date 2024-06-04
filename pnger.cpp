9d7a3886fab0177c8607c1c7e64133316bb40050
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-04 10:52:50 +0100

    pnger: reading signature

--- pnger.cpp
+++ pnger.cpp
@@ -1,9 +1,23 @@
 #pragma leco tool
 
+import traits;
 import yoyo;
 
+using namespace traits::ints;
+
+bool signature_matches(uint64_t hdr) { return hdr == 0x0A1A0A0D474E5089; }
+
+mno::req<void> dump_png(yoyo::reader &in) {
+  return in.read_u64()
+      .assert(signature_matches, "file signature doesn't match")
+      .map([](auto) {});
+}
+
 int main() {
-  return yoyo::file_reader::open("blank.png")
-      .map([](auto &) { return 0; })
-      .unwrap(1);
+  bool res = yoyo::file_reader::open("blank.png")
+                 .fmap(dump_png)
+                 .map([] { return true; })
+                 .log_error();
+
+  return res ? 0 : 1;
 }
