7ecbf49bad4cc569b63514b7f1fa6902417a07c3
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-04 10:58:38 +0100

    pnger: preparing to dump chunks

--- pnger.cpp
+++ pnger.cpp
@@ -7,10 +7,16 @@ using namespace traits::ints;
 
 bool signature_matches(uint64_t hdr) { return hdr == 0x0A1A0A0D474E5089; }
 
+mno::req<void> dump_chunk(yoyo::reader &in) {
+  return in.read_u32().map([](auto) {});
+}
+
 mno::req<void> dump_png(yoyo::reader &in) {
   return in.read_u64()
       .assert(signature_matches, "file signature doesn't match")
-      .map([](auto) {});
+      .map([](auto) {})
+      .until_failure([&] { return dump_chunk(in); },
+                     [&](auto) { return !in.eof().unwrap(false); });
 }
 
 int main() {
