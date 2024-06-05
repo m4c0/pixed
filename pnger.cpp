0da7ff958f970c21dfbfcee55428e8789fdf680c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-06-05 19:01:17 +0100

    pnger: refactor to reuse read chunk logic

--- pnger.cpp
+++ pnger.cpp
@@ -7,7 +7,12 @@ import traits;
 import yoyo;
 
 using namespace traits::ints;
-using chunk_t = hai::varray<uint8_t>;
+using chunk_data_t = hai::varray<uint8_t>;
+
+struct chunk {
+  uint32_t type;
+  chunk_data_t data;
+};
 
 static void usage() {
   silog::log(silog::error, R"(
@@ -30,44 +35,49 @@ static bool signature_matches(uint64_t hdr) {
   return hdr == 0x0A1A0A0D474E5089;
 }
 
-static mno::req<chunk_t> read_sPLT(chunk_t current, yoyo::reader &in) {
-  uint32_t type{};
-  chunk_t data{};
+static mno::req<chunk> read_chunk(yoyo::reader &in) {
+  chunk res{};
   return in.read_u32_be()
       .fmap([&](auto l) {
-        data.set_capacity(l);
-        data.expand(l);
+        res.data.set_capacity(l);
+        res.data.expand(l);
         return in.read_u32();
       })
       .fmap([&](auto t) {
-        type = t;
-        if (data.size() == 0)
+        res.type = t;
+        if (res.data.size() == 0)
           return mno::req<void>{};
 
-        return in.read(data.begin(), data.size());
+        return in.read(res.data.begin(), res.data.size());
       })
       .fmap([&] { return in.read_u32(); })
-      .map([&](auto crc) {
-        auto name =
-            jute::view::unsafe(reinterpret_cast<const char *>(data.begin()));
-        if (type == 'TLPs' && name == pal_name)
-          return traits::move(data);
-
-        return traits::move(current);
-      });
+      .map([&](auto crc) { return traits::move(res); });
 }
 
-static mno::req<chunk_t> find_sPLT_in_png(yoyo::reader &in) {
+static mno::req<chunk_data_t> read_sPLT(chunk_data_t current,
+                                        yoyo::reader &in) {
+  return read_chunk(in).map([&](chunk &c) {
+    auto &[type, data] = c;
+    auto name =
+        jute::view::unsafe(reinterpret_cast<const char *>(data.begin()));
+    if (type == 'TLPs' && name == pal_name)
+      return traits::move(data);
+
+    return traits::move(current);
+  });
+}
+
+static mno::req<chunk_data_t> find_sPLT_in_png(yoyo::reader &in) {
   return in.read_u64()
       .assert(signature_matches, "file signature doesn't match")
-      .map([](auto _signature) { return chunk_t{}; })
+      .map([](auto _signature) { return chunk_data_t{}; })
       .until_failure(
           [&](auto &&res) { return read_sPLT(traits::move(res), in); },
           [&](auto msg) { return !in.eof().unwrap(false); });
 }
 
-chunk_t new_sPLT() {
-  chunk_t res{initial_size};
+chunk_data_t new_sPLT() {
+  chunk_data_t res{initial_size};
   res.expand(initial_size);
 
   char *buf = reinterpret_cast<char *>(res.begin());
@@ -78,7 +88,7 @@ chunk_t new_sPLT() {
   return res;
 }
 
-void append_sPLT(chunk_t &sPLT, const char *val) {
+void append_sPLT(chunk_data_t &sPLT, const char *val) {
   if (sPLT.size() == 0) {
     silog::log(silog::error, "attempt of appending to a non-existing palette");
     throw 0;
@@ -108,14 +118,22 @@ void append_sPLT(chunk_t &sPLT, const char *val) {
   sPLT.push_back_doubling(0);
 }
 
+static mno::req<void> pass_chunk(yoyo::reader &in, yoyo::writer &out,
+                                 const chunk_data_t &sPLT) {
+  return mno::req<void>{};
+}
 static mno::req<void> replace_sPLT(yoyo::reader &in, yoyo::writer &out,
-                                   const chunk_t &sPLT) {
-  return {};
+                                   const chunk_data_t &sPLT) {
+  return in.read_u64()
+      .assert(signature_matches, "file signature doesn't match")
+      .fmap([&](auto signature) { return out.write(signature); })
+      .until_failure([&] { return pass_chunk(in, out, sPLT); },
+                     [&](auto msg) { return !in.eof().unwrap(false); });
 }
 
 int main(int argc, char **argv) try {
   mno::req<yoyo::file_reader> in{};
-  chunk_t sPLT{};
+  chunk_data_t sPLT{};
   auto opts = gopt_parse(argc, argv, "i:o:rna:", [&](auto ch, auto val) {
     switch (ch) {
     case 'a':
