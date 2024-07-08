ca5810bbd9eca4badbcba188f806d9c7d29e0217
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-08 19:10:54 +0100

    sprite mode: drawing a grid

--- atlased_sprmode.cpp
+++ atlased_sprmode.cpp
@@ -2,7 +2,34 @@ module atlased;
 
 static dotz::ivec2 g_cursor{};
 
-static unsigned data(quack::instance *i) { return 0; }
+static unsigned data(quack::instance *i) {
+  auto [sw, sh] = g_ctx.spr_size;
+  dotz::vec2 sz = g_ctx.spr_size;
+
+  quack::donald::push_constants({
+      .grid_pos = sz / 2.0f,
+      .grid_size = sz,
+  });
+
+  *i++ = {
+      .position = g_cursor,
+      .size = {1, 1},
+      .colour = {1, 0, 0, 1},
+  };
+
+  for (dotz::vec2 p{}; p.y < sh; p.y++) {
+    for (p.x = 0; p.x < sw; p.x++) {
+      *i++ = {
+          .position = p + 0.05f,
+          .size = dotz::vec2(0.9f),
+          .uv0 = p / sz,
+          .uv1 = (p + 1) / sz,
+          .multiplier = {1, 1, 1, 1},
+      };
+    }
+  }
+  return sw * sh + 1;
+}
 
 static void cursor(dotz::ivec2 d) {
   g_cursor = (g_cursor + d + g_ctx.spr_size) % g_ctx.spr_size;
