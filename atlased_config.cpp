0a3f0d4ad081a46e9013e0fa8ec5d6eb10091403
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-12 14:51:56 +0100

    config: storing cstr instead of an array buffer

--- atlased_config.cpp
+++ atlased_config.cpp
@@ -1,38 +1,47 @@
 module atlased;
 import buoy;
 import fork;
+import hai;
 import silog;
+import traits;
+import yoyo;
 
-static constexpr const auto max_name_len = 1024;
-static struct {
-  char name[max_name_len]{};
-} g_cur_file;
+static hai::cstr g_cur_file{};
+
+static auto read_cstr(yoyo::subreader sr) {
+  g_cur_file = hai::cstr{static_cast<unsigned>(sr.raw_size())};
+  return sr.read(g_cur_file.begin(), g_cur_file.size());
+}
+static auto read_image() {
+  return pixed::read(g_cur_file.begin())
+      .map([](auto &ctx) {
+        g_ctx = traits::move(ctx);
+        silog::log(silog::info, "Loaded [%s] based on config",
+                   g_cur_file.begin());
+      })
+      .trace("loading image from config file");
+}
 
 void atlased::config::load() {
   buoy::open_for_reading("pixed", "atlased")
       .fpeek(frk::assert("PXD"))
-      .fpeek(frk::take("fILE", &g_cur_file))
+      .fpeek(frk::take("fILE", read_cstr))
       .map(frk::end())
+      .fmap(read_image)
+      .trace("loading atlased config")
       .take(silog::log_failure);
 }
 
 static void write() {
   buoy::open_for_writing("pixed", "atlased")
       .fpeek(frk::signature("PXD"))
-      .fpeek(frk::chunk("fILE", g_cur_file))
+      .fpeek(frk::chunk("fILE", g_cur_file.begin(), g_cur_file.size()))
       .map(frk::end())
+      .trace("writing atlased config")
       .take(silog::log_failure);
 }
 
 void atlased::config::set_current_file(jute::view name) {
-  auto &f = g_cur_file.name;
-  unsigned n{};
-  for (auto c : name) {
-    if (n >= max_name_len)
-      return;
-
-    f[n++] = c;
-  }
-
+  g_cur_file = name.cstr();
   write();
 }
