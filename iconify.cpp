5abd687ca1d2259c8b346d17e42b32cdbc264fa7
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-30 17:19:50 +0100

    writing ICO with multiple sizes

--- iconify.cpp
+++ iconify.cpp
@@ -8,33 +8,53 @@ import yoyo;
 using namespace traits::ints;
 
 static mno::req<void> convert_to_ico(const pixed::context &ctx) {
-  constexpr const auto w = 16;
-  constexpr const auto h = 16;
+  constexpr const auto num_imgs = 3;
+
+  auto res = yoyo::file_writer::open("icon.ico")
+                 .fpeek(yoyo::write_u16(0))         // Reserved
+                 .fpeek(yoyo::write_u16(1))         // Type: icon
+                 .fpeek(yoyo::write_u16(num_imgs)); // Number of images
+  if (!res.is_valid())
+    return res.map([](auto &) {}).trace("writing ICO header");
 
   constexpr const auto hdr_sz = 6;
   constexpr const auto img_hdr_sz = 16;
-  constexpr const auto ofs = hdr_sz + img_hdr_sz;
 
-  auto icon = pixed::create(16, 16);
-  hai::array<uint8_t> buf{102400};
-  unsigned size{};
-  auto res = pixed::write(buf, icon)
-                 .map([&](auto s) { size = s; })
-                 .fmap([] { return yoyo::file_writer::open("icon.ico"); })
-                 .fpeek(yoyo::write_u16(0)) // Reserved
-                 .fpeek(yoyo::write_u16(1)) // Type: icon
-                 .fpeek(yoyo::write_u16(1)) // Number of images
+  constexpr const unsigned sizes[num_imgs]{64, 32, 16};
+  hai::varray<uint8_t> bufs[num_imgs]{};
+  unsigned ofs = hdr_sz + img_hdr_sz * num_imgs;
+  ;
+  auto *buf = bufs;
+  for (auto s : sizes) {
+    auto icon = pixed::create(s, s);
+    buf->set_capacity(102400);
 
-                 .fpeek(yoyo::write_u8(w))     // Width
-                 .fpeek(yoyo::write_u8(h))     // Height
-                 .fpeek(yoyo::write_u8(0))     // Num. of colours
-                 .fpeek(yoyo::write_u8(0))     // Reserved
-                 .fpeek(yoyo::write_u16(0))    // Colour planes
-                 .fpeek(yoyo::write_u16(32))   // Bits per pixel
-                 .fpeek(yoyo::write_u32(size)) // Image size
-                 .fpeek(yoyo::write_u32(ofs)); // Image offset
+    auto r = pixed::write(*buf, icon);
+    if (!r.is_valid())
+      return r.trace("writing downscaled image");
 
-  res = res.fpeek(yoyo::write(buf.begin(), size));
+    auto sz = buf->size();
+
+    res = res.fpeek(yoyo::write_u8(s))      // Width
+              .fpeek(yoyo::write_u8(s))     // Height
+              .fpeek(yoyo::write_u8(0))     // Num. of colours
+              .fpeek(yoyo::write_u8(0))     // Reserved
+              .fpeek(yoyo::write_u16(0))    // Colour planes
+              .fpeek(yoyo::write_u16(32))   // Bits per pixel
+              .fpeek(yoyo::write_u32(sz))   // Image size
+              .fpeek(yoyo::write_u32(ofs)); // Image offset
+    if (!res.is_valid())
+      return res.map([](auto &) {}).trace("writing image subheader");
+
+    ofs += sz;
+    buf++;
+  }
+
+  for (auto &buf : bufs) {
+    res = res.fpeek(yoyo::write(buf.begin(), buf.size()));
+    if (!res.is_valid())
+      return res.map([](auto &) {}).trace("writing image data");
+  }
 
   return res.map([](auto &) {});
 }
